f2df47871350cd540d0c8d44b0eefb8f53ce7ea3
NaheedRayan <naheed28ray@protonmail.com>
2021-12-16 13:01:16 +0600

    from bot1

--- commit_bot_answer.sh
+++ commit_bot_answer.sh
@@ -8,7 +8,9 @@ spawn ./commit_bot.sh
 expect "Username for 'https://github.com': "
 send -- "NaheedRayan\r"
 
-expect "Password "
+expect "Password for 'https://NaheedRayan@github.com': "
+
+send "$expect_out(0,string)"
 send -- "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\r"
 
 
