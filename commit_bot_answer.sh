85dc4daebcf377cad566a62848cfcd02a6b255ca
NaheedRayan <naheed28ray@protonmail.com>
2021-12-17 23:32:35 +0600

    from bot1

--- commit_bot_answer.sh
+++ commit_bot_answer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect -f
 
 
-set timeout -1
+set timeout 50
 
 spawn ./commit_bot.sh
 
@@ -9,7 +9,7 @@ spawn ./commit_bot.sh
 expect "Username for 'https://github.com': "
 send -- "NaheedRayan\r"
 
-
+set timeout 50
 expect "Password for 'https://NaheedRayan@github.com': "
 send -- "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\n"
 interact
